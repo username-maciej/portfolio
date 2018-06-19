d412a5eb98607069b7a16fb621c92d1922608079
username-maciej <m.michna.official@gmail.com>
2018-06-19 11:36:03 +0200

    contact form validation added

--- mail.php
+++ mail.php
@@ -1,11 +1,15 @@
 <?php
-$to      = 'm.michna.official@gmail.com';
-$name    = $_POST['name'];
-$email   = $_POST['email'];
-$message = $_POST['message'];
-$subject = 'Email z portfolio od' . $name;
-$headers = 'From:' $email . "\r\n";
-$headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
-mail($to, $subject, $message, $headers);
-?>
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $mailTO = "m.michna.official@gmail.com";
+    $headers = "From: ".$emial;
+    $text = "You have received a mail from" .$name."\n\n".$message;
+    
+    
+    mail($mailTO, $text, $headers);
+    header("Location: index.php?mailsend")
+}
